be47d609ca3336127007afa1d6c3af19bafffb26
Carlos Vargas <varlopecar@gmail.com>
2024-01-17 09:55:23 +0100

    style(theme): create and config globals

--- ypursuit/tailwind.config.ts
+++ ypursuit/tailwind.config.ts
@@ -1,20 +1,48 @@
-import type { Config } from 'tailwindcss'
+import type { Config } from "tailwindcss";
 
 const config: Config = {
   content: [
-    './pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './components/**/*.{js,ts,jsx,tsx,mdx}',
-    './app/**/*.{js,ts,jsx,tsx,mdx}',
+    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
+    screens: {
+      sm: "480px",
+      md: "768px",
+      lg: "976px",
+      xl: "1440px",
+    },
+    colors: {
+      white: "#ffffff",
+      black: "#000000",
+      greenPrimary: "#1AB2A5",
+      greenSecondary: "#6CD200",
+      red: "#F20D0D",
+      blue: "#00CBDD",
+      orange: "#F07D1A",
+      pinkGradient: {
+        light: "#FF00FF",
+        dark: "#B12BFF",
+      },
+      greenGradient: {
+        light: "#23B2A4",
+        dark: "#3CA55B",
+      },
+    },
+    fontFamily: {
+      sans: ["Montserrat", "sans-serif"],
+      serif: ["Montserrat", "sans-serif"],
+      mono: ["Montserrat", "sans-serif"],
+    },
     extend: {
       backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
+        "gradient-conic":
+          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
       },
     },
   },
   plugins: [],
-}
-export default config
+};
+export default config;
