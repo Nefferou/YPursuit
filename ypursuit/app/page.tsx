46fb29b71967b4d0ac9ed29e69dcc001649670ea
Carlos Vargas <varlopecar@gmail.com>
2024-02-07 14:32:15 +0100

    style(button): show an icon in a button

--- ypursuit/app/page.tsx
+++ ypursuit/app/page.tsx
@@ -1,13 +1,26 @@
 'use client';
 import React from 'react';
 import MainScroll from '../components/MainScroll';
-
+import SignInForm from '@/components/LoginForm';
+import SignUpForm from '@/components/auth/SignUpForm';
+import Card from '@/components/Card';
+import Button from '@/components/ui/Buttons/Button';
+import Insta from '@/assets/images/Insta.svg';
 
 export default function Home() {
   return (
-    <main>
-      <MainScroll />
+    <main className='p-8'>
+      <Button
+        handleClick={() => console.log('Button')}
+        title="Button"
+        design="double"
+        backgroundColor="green"
+        type="button"
+        disabled={false}
+        icon={Insta}
+      >
+        Button
+      </Button>
     </main>
   );
 }
-  
