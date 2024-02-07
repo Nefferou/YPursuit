fe1cb2467895f3d74fa053258f56fc6b5873a205
LamaShin <124254584+LamaShin@users.noreply.github.com>
2024-02-07 16:47:09 +0100

    [~] Ajout Handle hover iamge

--- ypursuit/components/CharacterSlide/index.tsx
+++ ypursuit/components/CharacterSlide/index.tsx
@@ -1,17 +1,31 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Image from 'next/image';
-import Card from '@/assets/images/card/artchyPile.svg';
-import Card2 from '@/assets/images/card/buzzyPile.svg';
-
+import CardArtchy from '@/assets/images/card/artchyPile.svg';
+import CardBuzzy from '@/assets/images/card/buzzyPile.svg';
+import CardArtchyFace from '@/assets/images/card/artchyFace.svg'
+import CardBuzzyFace from '@/assets/images/card/buzzyFace.svg'
 
 const CharacterSlide = () => {
+    const [isHovered1, setIsHovered1] = useState(false);
+    const [isHovered2, setIsHovered2] = useState(false);
+
     return (
         <div id="slide4" className="w-full h-screen flex justify-center items-center border-y border-black">
-            <div className='w-1/2 h-1/2 flex'>
-                <Image src={Card} alt="Logo" />
+            <div className='w-1/2 h-1/2 flex transition-transform transform-gpu hover:rotate-[360deg]'>
+                <Image 
+                    src={isHovered1 ? CardArtchyFace : CardArtchy } 
+                    alt="Logo"
+                    onMouseEnter={() => setIsHovered1(true)}
+                    onMouseLeave={() => setIsHovered1(false)}
+                />
             </div>
-            <div className='w-1/2 h-1/2 flex'>
-                <Image src={Card2} alt="Logo" />
+            <div className='w-1/2 h-1/2 flex transition-transform transform-gpu hover:rotate-[360deg]'>
+                <Image 
+                    src={isHovered2 ? CardBuzzyFace : CardBuzzy } 
+                    alt="Logo" 
+                    onMouseEnter={() => setIsHovered2(true)}
+                    onMouseLeave={() => setIsHovered2(false)}
+                />
             </div>
         </div>
     );
