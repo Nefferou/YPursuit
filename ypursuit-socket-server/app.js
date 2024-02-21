08e74f05d84fbdac00bae646d4951ee3bfe9f765
Arseid <yvesrobert.estrada@gmail.com>
2024-02-21 16:39:41 +0100

    Room features join_room, retrieve_room_info, leave_room, kick_player

--- ypursuit-socket-server/app.js
+++ ypursuit-socket-server/app.js
@@ -23,10 +23,81 @@ io.on('connection', (socket) => {
     // ------------------ Handle room creation ------------------
     socket.on('create_room', ({ name, maxPlayers, difficulty }) => {
         const roomId = generateRoomId();
-        const room = { id: roomId, name, maxPlayers, difficulty, players: [socket.id] };
+        const room = { id: roomId, name, maxPlayers, difficulty, players: [{id: socket.id, isHost: true}] };
         rooms.push(room);
         console.log(`Room created with ID: ${roomId}`);
+        socket.emit('join_room', { roomId });
+        socket.emit('room_created', roomId);
         io.emit('update_rooms', rooms);
+    });
+
+    // ------------------ Handle joining a room ------------------
+    socket.on('join_room', ({ roomId }) => {
+        const room = rooms.find(room => room.id === roomId);
+        if (room && room.players.length < room.maxPlayers) {
+            socket.join(roomId);
+            room.players.push({ id: socket.id, isHost: room.players.length === 0 });
+            io.in(roomId).emit('update_room', room);
+            io.emit('update_rooms', rooms);
+            socket.emit('room_join_response', true);
+        } else {
+            socket.emit('room_join_response', false);
+        }
+    });
+
+    // ------------------ Handle retrieve info of a room ------------------
+    socket.on('retrieve_room_info', ({ roomId }) => {
+        const room = rooms.find(room => room.id === roomId);
+        if (room) {
+            socket.emit('update_room', room);
+        }
+    });
+
+    // ------------------ Handle leaving a room ------------------
+    socket.on('leave_room', ({ roomId }) => {
+        const room = rooms.find(room => room.id === roomId);
+        if (room) {
+            socket.leave(roomId);
+            const index = room.players.findIndex(player => player.id === socket.id);
+            let isHost = false;
+            if (index !== -1) {
+                isHost = room.players[index].isHost;
+                room.players.splice(index, 1);
+                if (room.players.length === 0) {
+                    // If no players are left, delete the room
+                    rooms = rooms.filter(room => room.id !== roomId);
+                    console.log(`Room ${roomId} is deleted`);
+                } else{
+                    if (isHost) {
+                        // Pass host to the next player
+                        room.players[0].isHost = true;
+                    }
+                }
+            }
+            io.in(roomId).emit('update_room', room);
+            io.emit('update_rooms', rooms);
+        }
+    });
+
+    // ------------------ Handle kicking a player ------------------
+    socket.on('kick_player', ({ roomId, playerId }) => {
+        const room = rooms.find(room => room.id === roomId);
+        if (room) {
+            const player = room.players.find(player => player.id === socket.id);
+            if (player && player.isHost) {
+                const index = room.players.findIndex(player => player.id === playerId);
+                if (index !== -1) {
+                    room.players.splice(index, 1);
+                    if (index === 0 && room.players.length > 0) {
+                        // If host was kicked, pass host
+                        room.players[0].isHost = true;
+                    }
+                    socket.to(playerId).emit('kicked');
+                    io.in(roomId).emit('update_room', room);
+                    io.emit('update_rooms', rooms);
+                }
+            }
+        }
     });
 
     // ------------------ Handle disconnection ------------------
