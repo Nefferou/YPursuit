5423c5ee1e558a9290f04554697405b07bee3704
Nefferou <fertijulien@gmail.com>
2024-02-21 14:52:51 +0100

    Add rout Play -> Solo / Multiplayer -> Join / Host

---
+++ ypursuit/app/play/multiplayer/join/page.tsx
@@ -0,0 +1,7 @@
+export default function Home() {
+  return (
+    <main>
+      <h1>Join Game</h1>
+    </main>
+  );
+}
---
+++ ypursuit/app/play/multiplayer/page.tsx
@@ -0,0 +1,7 @@
+export default function Home() {
+  return (
+    <main>
+      <h1>Join || Host</h1>
+    </main>
+  );
+}
