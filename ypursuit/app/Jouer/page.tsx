df98c41b7d5319216dfb2c0fdd1608c7074aa1ba
LamaShin <c.blairon-lacoste@hotmail.fr>
2024-02-21 12:42:18 +0100

    [+]AJout components HeaderClassic pour navigation simple

---
+++ ypursuit/app/Jouer/page.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import HeaderClassic from '../../components/HeaderClassic';
+
+export default function Home() {
+  return (
+    <main>
+        <HeaderClassic />
+    </main>
+  );
+}
