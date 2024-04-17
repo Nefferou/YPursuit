da31dffd7195c523ff4669fe0382f480cf73f5a3
LamaShin <c.blairon-lacoste@hotmail.fr>
2024-04-17 15:12:02 +0200

    [~] changement css

--- ypursuit/app/play/multiplayer/page.tsx
+++ ypursuit/app/play/multiplayer/page.tsx
@@ -7,6 +7,7 @@ import Buzzy from "@/assets/images/character/buzzy.svg";
 import Dally from "@/assets/images/character/dally.svg";
 import Rusty from "@/assets/images/character/rusty.svg";
 import Wapy from "@/assets/images/character/wapy.svg";
+import HeroBg from '@/assets/images/hero.svg';
 import Image from "next/image";
 
 export default function Home() {
@@ -14,23 +15,24 @@ export default function Home() {
   const router = useRouter();
 
   return (
-    <main className="relative flex flex-col items-center justify-center w-full h-full p-4">
-    <h1 className="text-2xl font-extrabold italic">MultiJoueur</h1>
-    <BackButton />
-    <div className="flex justify-between w-full">
-      <div className="flex items-center">
-        <Image src={Rusty} alt="rusty" width={180} height={75} className="brightness-0" />
+    <main className="relative flex flex-col items-center justify-evenly w-full h-full p-4">
+      <div className="h-20 w-screen bg-cover" style={{ backgroundImage: `url(${HeroBg})` }}></div>
+      <h1 className="text-4xl font-extrabold underline">MultiJoueur</h1>
+      <BackButton />
+      <div className="flex justify-between w-full">
+        <div className="flex items-center">
+          <Image src={Rusty} alt="rusty" width={240} height={100} className="brightness-0" />
+        </div>
+        <div className="flex items-center">
+          <Image src={Wapy} alt="wapy" width={180} height={75} className="brightness-0" />
+          <Image src={Buzzy} alt="buzzy" width={180} height={75} className="brightness-0" />
+          <Image src={Dally} alt="dally" width={240} height={100} className="brightness-0" />
+        </div>
       </div>
-      <div className="flex items-center">
-        <Image src={Wapy} alt="wapy" width={180} height={75} className="brightness-0"/>
-        <Image src={Buzzy} alt="buzzy" width={180} height={75} className="brightness-0"/>
-        <Image src={Dally} alt="dally" width={180} height={75} className="brightness-0"/>
+      <div className="relative z-10" style={{ display: "flex", gap: "100px" }}>
+        <Button handleClick={() => { router.push('multiplayer/join') }} title="Button" design="simple">Créer une room</Button>
+        <Button handleClick={() => { router.push('multiplayer/host') }} title="Button" design="simple">Joindre une room</Button>
       </div>
-    </div>
-    <div className="relative z-10" style={{ display: "flex", gap: "100px" }}>
-      <Button handleClick={() => { router.push('multiplayer/join') }} title="Button" design="simple">Créer une room</Button>
-      <Button handleClick={() => { router.push('multiplayer/host') }} title="Button" design="simple">Joindre une room</Button>
-    </div>
-  </main>
+    </main>
   );
 }
